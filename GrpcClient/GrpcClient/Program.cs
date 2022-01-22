d0945ffc505e43991884d79540665379a0d3b96d
Arindam <arindam.datta@decos.com>
2022-01-22 19:35:22 +0530

    Add project files.

---
+++ GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,80 @@
+﻿using Grpc.Net.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UserManagement.Privileges;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static string gRPCServerAdddress = "https://localhost:5001";
+
+        static GrpcChannel Channel = null;
+        static GrpcChannel GetChannel()
+        {
+            if (Channel == null)
+            {
+                Channel = GrpcChannel.ForAddress(gRPCServerAdddress);
+            }
+            return Channel;
+        }
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press any key to continue........");
+            Console.ReadLine();
+            var reply = await GetHelloReply();
+            Console.WriteLine($"Got reply: {reply.Message} {reply.TimeOfDay}");
+
+            Console.WriteLine("Press any key to continue........");
+            Console.ReadLine();
+            await GetPrivilegeSetsStream();
+
+            Console.WriteLine("Press any key to continue........");
+            Console.ReadLine();
+            await GetPrivilegeSetResponseObject();
+
+            Console.ReadLine();
+        }
+
+        static async Task GetPrivilegeSetResponseObject()
+        {
+            var channel = GetChannel();
+            var client = new UserManagement.Privileges.PrivilegeSetService.PrivilegeSetServiceClient(channel);
+            var reply = await client.GetPrivilegeSetListAsync(new GetPrivilegeSetsRequest());
+            foreach(var ps in reply.Items)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(ps));
+            }
+        }
+        static async Task GetPrivilegeSetsStream()
+        {
+            var channel = GetChannel();
+            var client = new UserManagement.Privileges.PrivilegeSetService.PrivilegeSetServiceClient(channel);
+            var reply = client.GetPrivilegeSets(new UserManagement.Privileges.GetPrivilegeSetsRequest());
+            await foreach (var ps in ReadStreamAsync(reply.ResponseStream))
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(ps));
+            }
+
+        }
+        static async IAsyncEnumerable<T> ReadStreamAsync<T>(Grpc.Core.IAsyncStreamReader<T> streamReader)
+        {
+            // you can see the documentation for Grpc.Core.IAsyncStremReader
+            while (await streamReader.MoveNext(CancellationToken.None))
+            {
+                yield return streamReader.Current;
+            }
+            yield break;
+        }
+        static async Task<HelloReply> GetHelloReply()
+        {
+            var channel = GetChannel();
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Arindam" });
+            return reply;
+        }
+    }
+}
---
+++ Phase1/GrpcService/Services/PrivilegeSetGrpcService.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UserManagement.Privileges;
+
+namespace GrpcService.Services
+{
+    public class PrivilegeSetGrpcService : PrivilegeSetService.PrivilegeSetServiceBase
+    {
+        public override async Task GetPrivilegeSets(GetPrivilegeSetsRequest request, IServerStreamWriter<PrivilegeSet> responseStream, ServerCallContext context)
+        {
+            var dbPrivilegeSets = await GetAllDatabasePrivileges();
+            dbPrivilegeSets.ForEach(async (ps) => await responseStream.WriteAsync(ps));
+        }
+        public override async Task<PrivilegeSetListResponse> GetPrivilegeSetList(GetPrivilegeSetsRequest request, ServerCallContext context)
+        {
+            var response = new PrivilegeSetListResponse();
+            var dbPrivilegeSets = await GetAllDatabasePrivileges();
+            response.Items.AddRange(dbPrivilegeSets);
+            return response;
+        }
+
+        private Task<List<PrivilegeSet>> GetAllDatabasePrivileges()
+        {
+            var dbPrivilegeSets = new List<PrivilegeSet>();
+            var allowId = "440a9616-d1bf-4528-a6e7-6cb40533a195";
+            var ps1 = new PrivilegeSet
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Privilege-Set-1"
+            };
+            ps1.Privileges.Add(new Privilege
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Manage Session",
+                AccessLevel = new AccessLevel { Id = allowId, Name = "Allow" }
+            });
+            var ps2 = new PrivilegeSet
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Privilege-Set-2"
+            };
+            ps2.Privileges.Add(new Privilege
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Manage Projects",
+                AccessLevel = new AccessLevel { Id = allowId, Name = "Allow" }
+            });
+
+            dbPrivilegeSets.AddRange(new[] { ps1, ps2 });
+            return Task.FromResult(dbPrivilegeSets);
+        }
+    }
+}
